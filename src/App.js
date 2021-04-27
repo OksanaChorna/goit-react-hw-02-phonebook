b18c75e1d6274446917c4a9f02a60f78169e7f3a
OksanaChorna <ksuchorna@gmail.com>
2021-04-27 17:23:36 +0300

    Створепо репозиторій

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Container from './components/Container';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Container>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
