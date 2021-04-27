08e9be531f1da89650fc00afe363283f7d26e8be
OksanaChorna <ksuchorna@gmail.com>
2021-04-27 19:07:34 +0300

    Додано форму, прописано сабміт

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
+import React, { Component } from "react";
 import Container from './components/Container';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  state = {
+  contacts: [],
+  name: ''
+  }
+
+  handleInputChange = event => {
+// console.log(event.target.value)
+    this.setState({ name: event.target.value })
+  }
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({[name]: value})
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state);
+
+  }
+
+  render() {
+    return (
       <Container>
-      </Container>
-    </div>
-  );
+        <h1>PhoneBook</h1>
+
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="">
+            Name
+            <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+            required
+            value={this.state.name}
+            onChange={this.handleChange}
+              />
+        </label>
+          <button type="submit">Add contact</button>
+        </form>
+        <div>
+          <h2>Contacts</h2>
+          <ul><li></li></ul>
+        </div>
+
+        </Container>
+    )
+  }
+  
 }
 
 export default App;
---
+++ src/components/PhoneBook/PhoneBook.js
@@ -0,0 +1,10 @@
+// import PropsType from 'prop-types';
+// import style from './PhoneBook.module.css';
+
+const PhoneBook = () => {
+    return (
+        <div>
+            
+        </div>
+)
+}
