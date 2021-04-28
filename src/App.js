dbdc77b8ede16e82ec811e1e3ff4d07ae549550f
OksanaChorna <ksuchorna@gmail.com>
2021-04-28 13:30:20 +0300

    Початок фільтра

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ import Container from './components/Container';
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
     name: '',
     number: '',
   };
@@ -21,6 +27,10 @@ class App extends Component {
   //     contacts: prevState.contacts.filter(contact => contact.id !== contactId),
   //   }));
   // }; // 1.47 vebinar 3, на кнопку Видалити
+
+  findContact = event => {
+    event.preventDefault();
+  };
 
   handleChange = event => {
     const { name, value } = event.currentTarget;
@@ -75,6 +85,13 @@ class App extends Component {
         </form>
         <div>
           <h2>Contacts</h2>
+          <h3>Find contacts by name</h3>
+          <input
+            type="text"
+            name="filter"
+            value={this.state.filter}
+            onChange={this.handleChange}
+          />
           <ul>
             {this.state.contacts.map(({ id, name, number }) => {
               return (
